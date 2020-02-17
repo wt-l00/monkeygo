b1040c8470da83f3730d280ef723e942922f1803
wt-l00 <ei13suke@gmail.com>
2020-02-17 19:22:52 +0900

    first commit

---
+++ token/token.go
@@ -0,0 +1,46 @@
+package token
+
+// TokenType is string
+type TokenType string
+
+// Token consists of Type and Literal
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	// ILLEGAL means unknown
+	ILLEGAL = "ILLEGAL"
+	// EOF means end of file
+	EOF = "EOF"
+
+	// IDENT is identifier
+	IDENT = "IDENT"
+	// INT is integer
+	INT = "INT"
+
+	// ASSIGN is =
+	ASSIGN = "="
+	// PLUS is +
+	PLUS = "+"
+
+	// COMMA is ,
+	COMMA = ","
+	// SEMICOLON is ;
+	SEMICOLON = ";"
+
+	// LPAREN is (
+	LPAREN = "("
+	// RPAREN is )
+	RPAREN = ")"
+	// LBRACE is {
+	LBRACE = "{"
+	// RBRACE is }
+	RBRACE = "}"
+
+	// FUNCTION is func
+	FUNCTION = "FUNCTION"
+	// LET is LET
+	LET = "LET"
+)
