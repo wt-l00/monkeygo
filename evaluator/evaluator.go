e417e2e6fa80594a85d17b0cbc7d16b0c3981069
wt-l00 <ei13suke@gmail.com>
2020-04-14 14:41:23 +0900

    fix

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -88,6 +88,10 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return nativeBooleanObject(left == right)
+	case operator == "!=":
+		return nativeBooleanObject(left != right)
 	default:
 		return NULL
 	}
@@ -106,6 +110,14 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 		return &object.Integer{Value: leftVal * rightVal}
 	case "/":
 		return &object.Integer{Value: leftVal / rightVal}
+	case "<":
+		return nativeBooleanObject(leftVal < rightVal)
+	case ">":
+		return nativeBooleanObject(leftVal > rightVal)
+	case "==":
+		return nativeBooleanObject(leftVal == rightVal)
+	case "!=":
+		return nativeBooleanObject(leftVal != rightVal)
 	default:
 		return NULL
 	}
